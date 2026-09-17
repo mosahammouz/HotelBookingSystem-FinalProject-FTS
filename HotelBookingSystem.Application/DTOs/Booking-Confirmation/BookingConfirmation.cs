e0afc997c991d666b98cb489c38c9d7099e30cbe
Mousa Hammouz <mosahammouz2004@gmail.com>
2026-09-17 13:06:22 +0300

    created BookingConfirmation dto

---
+++ HotelBookingSystem.Application/DTOs/Booking-Confirmation/BookingConfirmation.cs
@@ -0,0 +1,23 @@
+namespace HotelBookingSystem.Application.DTOs.Booking_Confirmation;
+
+public class BookingConfirmation
+{
+    
+    public string ConfirmationNumber { get; set; } = string.Empty;
+
+    public string HotelName { get; set; } = string.Empty;
+    public string HotelAddress { get; set; } = string.Empty;
+
+    public int RoomId { get; set; }
+    public string RoomNumber { get; set; } = string.Empty;
+    public string RoomType { get; set; } = string.Empty;
+
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+
+    public decimal PricePerNight { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    public string PaymentStatus { get; set; } = string.Empty;
+    public string BookingStatus { get; set; } = string.Empty;
+}
