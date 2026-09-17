af5d3546ac1a2354a14bf1bf925d9393938d63d5
Mousa Hammouz <mosahammouz2004@gmail.com>
2026-09-17 13:12:17 +0300

    implemented ConfirmationRepository

--- HotelBookingSystem.Infrastructure/Persistence/Repositories/ConfirmationRepository.cs
+++ HotelBookingSystem.Infrastructure/Persistence/Repositories/ConfirmationRepository.cs
@@ -1,6 +1,26 @@
+using HotelBookingSystem.Domain.Entities;
+using HotelBookingSystem.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace HotelBookingSystem.Infrastructure.Persistence.Repositories;
 
-public class ConfirmationRepository
+public class ConfirmationRepository : IConfirmationRepository
 {
-    
+    private readonly AppDbContext _dbContext;
+    public ConfirmationRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Booking?> GetBookingForConfirmationAsync(int userId, int bookingId)
+    {
+        return await _dbContext.Bookings
+            .Include(b => b.BookingRooms)
+            .ThenInclude(br => br.Room)
+            .ThenInclude(r => r.Hotel)
+            .Include(b => b.Payment)
+            .FirstOrDefaultAsync(b =>
+                b.Id == bookingId &&
+                b.UserId == userId);
+    }
 }
