dd0f714474fc4548a71f969d9b6db2346ab2dd19
Mousa Hammouz <mosahammouz2004@gmail.com>
2026-09-18 18:44:26 +0300

    customeWebApp Factory is don in integration test

--- HotelBookingSystem.Tests/Integration/CustomWebApplicationFactory.cs
+++ HotelBookingSystem.Tests/Integration/CustomWebApplicationFactory.cs
@@ -1,12 +1,34 @@
+using HotelBookingSystem.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace HotelBookingSystem.Tests.Integration;
 
-public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+public class CustomWebApplicationFactory
+    : WebApplicationFactory<Program>
 {
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    protected override void ConfigureWebHost(
+        IWebHostBuilder builder)
     {
         builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(
+                    "Host=localhost;Port=5432;" +
+                    "Database=HotelBookingSystem_Test;" +
+                    "Username=postgres;Password=abc123"));
+        });
     }
 }
